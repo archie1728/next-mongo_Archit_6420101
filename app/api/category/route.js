79ff6f79f6fc1765a878ca71b939e3a8161e12ad
archie1728 <archit1379@gmail.com>
2024-09-12 16:01:34 +0700

    Catergory changes

--- app/api/category/route.js
+++ app/api/category/route.js
@@ -1,7 +1,7 @@
 import Category from "@/models/Category";
 
 export async function GET() {
-  const categories = await Category.find().sort({ name: 1 })
+  const categories = await Category.find()
   return Response.json(categories)
 }
 
@@ -12,11 +12,8 @@ export async function POST(request) {
   return Response.json(category)
 }
 
-
-
-// for V2
 export async function PUT(request) {
   const body = await request.json()
-  const category = await Category.findByIdAndUpdate(body._id, body) 
+  const category = await Category.findByIdAndUpdate(body._id, body)
   return Response.json(category)
 }
--- models/Category.js
+++ models/Category.js
@@ -5,7 +5,9 @@ const categorySchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  order: Number
 });
+ 
 
 const Category = mongoose.models.category || mongoose.model("category", categorySchema);
 
